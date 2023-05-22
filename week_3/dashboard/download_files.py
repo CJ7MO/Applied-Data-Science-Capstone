5a1902979c820b62e8f40fd18b33b324394005f8
CJ7MO <104647401+CJ7MO@users.noreply.github.com>
2023-05-22 17:13:17 -0500

    Delete week_3 directory

--- week_3/dashboard/download_files.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/labs/module_3/spacex_dash_app.py"
-filename = "spacex_dash_app.py"
-url1 = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/spacex_launch_dash.csv"
-filename1 = "spacex_launch_dash.csv"
-urllib.request.urlretrieve(url, filename)
-urllib.request.urlretrieve(url1, filename1)
